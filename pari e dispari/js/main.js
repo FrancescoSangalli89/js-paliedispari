29a05aab5bcb636ba418819d4430f6fda979e274
Francesco Sangalli <francesco.sangalli89@gmail.com>
2022-07-12 20:15:45 +0200

    finish: exercise odd and even

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,57 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+const playerChoice = prompt('scegli pari o dispari');
+
+const playerNumber = parseInt(prompt('Scegli un numero tra 1 e 5'));
+
+const minComputer = 1;
+
+const maxComputer = 5;
+
+const randomNumberResult = randomNumber(minComputer, maxComputer);
+console.log('numero computer: ' + randomNumberResult);
+
+function randomNumber(min, max) {
+
+    if (playerNumber >= 1 && playerNumber <= 5) {
+
+        const computerNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+        console.log('numero computer: ' + computerNumber);
+    
+        return computerNumber;
+    } else {
+        alert('Inserisci un numero tra 1 e 5');
+    }
+
+}
+
+const sum = playerNumber + randomNumberResult;
+console.log(sum);
+
+const oddEvenResult = oddEven(sum);
+
+
+function oddEven(numberSum) {
+    
+    let result;
+
+    if (numberSum % 2 == 0) {
+        result = 'pari';
+    } else {
+        result = 'dispari';
+    }
+
+    return result;
+}
+
+if (oddEvenResult == playerChoice) {
+    alert ('Hai vinto tu!');
+} else {
+    alert('Ha vinto il computer!');
+}
